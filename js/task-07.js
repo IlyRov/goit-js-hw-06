dec43feca9570cf62f80bffb91f0d434aa062871
IlyRov <127045335+IlyRov@users.noreply.github.com>
2023-08-20 16:33:29 +0100

    гзвфеу

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,13 @@
 const fontSizeControl = document.getElementById('font-size-control');
-  const textElement = document.getElementById('text');
+const textElement = document.getElementById('text');
 
-  fontSizeControl.addEventListener('input', () => {
-    const fontSize = fontSizeControl.value + 'px';
-    textElement.style.fontSize = fontSize;
-  });
+fontSizeControl.addEventListener('input', () => {
+  const minFontSize = 16; 
+  const maxFontSize = 96; 
+  const inputValue = fontSizeControl.value;
+  
+
+  const fontSize = minFontSize + ((maxFontSize - minFontSize) * (inputValue - fontSizeControl.min)) / (fontSizeControl.max - fontSizeControl.min);
+  
+  textElement.style.fontSize = fontSize + 'px';
+});
